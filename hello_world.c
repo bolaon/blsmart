dc63d60d23165d781b0966482bf430b4917c2207
ZHANG'JIANAN <bolaon@163.com>
2019-11-01 23:05:20 +0800

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : hello_world.c
+ Author      : www
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    printf("hello world! ");
+	return EXIT_SUCCESS;
+}
